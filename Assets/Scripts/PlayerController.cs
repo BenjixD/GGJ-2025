e22cdb258686523a1323b3f0163a6130e8a77bee
Grace Zhao <gracezhao@Graces-MacBook-Pro.local>
2025-01-19 20:07:35 -0500

    added player sprint

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,8 +6,9 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     private Vector2 moveInput;
 
-    [Header("Camera movement")]
+    [Header("Camera")]
     public Camera playerCamera;
+    public float fov = 60f;
     public float mouseSensitivity = 3.0f;
     public float maxLookAngle = 60.0f;
     public bool lockCursor = true;
@@ -15,10 +16,17 @@ public class PlayerController : MonoBehaviour
     private float yaw = 0.0f;
     private float pitch = 0.0f;
 
-    [Header("Player movement")]
-    
+    [Header("Player Movement")]
     public float walkSpeed = 5.0f;
     public float maxVelocityChange = 10.0f;
+
+    [Header("Sprint")]
+    public float sprintSpeed = 10.0f;
+    public float sprintFOV = 90.0f;
+    public float sprintFOVStepTime = 10f;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+
+    private bool isSprinting = false;
 
     [Header("Jump")]
     public float jumpPower = 5f;
@@ -27,6 +35,7 @@ public class PlayerController : MonoBehaviour
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        playerCamera.fieldOfView = fov;
     }
 
     void Start()
@@ -44,11 +53,20 @@ public class PlayerController : MonoBehaviour
         yaw = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * mouseSensitivity;
         pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
 
-        // clamp between look angle
+        // clamp between look angles
         pitch = Mathf.Clamp(pitch, -maxLookAngle, maxLookAngle);
 
         transform.localEulerAngles = new Vector3(0.0f, yaw, 0.0f);
         playerCamera.transform.localEulerAngles = new Vector3(pitch, 0.0f, 0.0f);
+
+        if (isSprinting)
+        {
+            playerCamera.fieldOfView = Mathf.Lerp(playerCamera.fieldOfView, sprintFOV, sprintFOVStepTime * Time.deltaTime);
+        }
+        else
+        {
+            playerCamera.fieldOfView = Mathf.Lerp(playerCamera.fieldOfView, fov, sprintFOVStepTime * Time.deltaTime);
+        }
 
         CheckGround();
 
@@ -56,21 +74,41 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        Walk();
-    }
-
-    private void Walk()
-    {
         Vector3 targetVelocity = new Vector3(moveInput.x, 0, moveInput.y);
-        targetVelocity = transform.TransformDirection(targetVelocity) * walkSpeed;
-        Vector3 velocity = rb.linearVelocity;
+        
+        // Sprint
+        if (Input.GetKey(sprintKey)) // old input
+        {
+            targetVelocity = transform.TransformDirection(targetVelocity) * sprintSpeed;
 
-        Vector3 velocityChange = (targetVelocity - velocity);
-        velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
-        velocityChange.y = 0;
-        velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+            Vector3 velocity = rb.linearVelocity;
+            Vector3 velocityChange = (targetVelocity - velocity);    
 
-        rb.AddForce(velocityChange, ForceMode.VelocityChange);
+            velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+            velocityChange.y = 0;
+            velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+            rb.AddForce(velocityChange, ForceMode.VelocityChange);
+            
+            if (velocityChange.x != 0 || velocityChange.z != 0)
+            {
+                isSprinting = true;
+            }
+        }
+        // Walk
+        else
+        {
+            isSprinting = false;
+
+            targetVelocity = transform.TransformDirection(targetVelocity) * walkSpeed;
+
+            Vector3 velocity = rb.linearVelocity;
+            Vector3 velocityChange = (targetVelocity - velocity);
+
+            velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+            velocityChange.y = 0;
+            velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+            rb.AddForce(velocityChange, ForceMode.VelocityChange);
+        }
     }
 
     private void OnMove(InputValue value)
