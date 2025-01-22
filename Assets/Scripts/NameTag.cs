1d8f29f4b8b5f65f3c41871509b0435bcd57def9
Yilun Zhao <yl4zhao@uwaterloo.ca>
2025-01-21 23:12:20 -0500

    Name tag finished

--- Assets/Scripts/NameTag.cs
+++ Assets/Scripts/NameTag.cs
@@ -6,8 +6,8 @@ public class NameTag: MonoBehaviour {
     private PlayerController player;
     private TMP_Text textMesh;
     private Color pastelBase = new Color(0xD7 / 255f, 0xF8 / 255f, 0xFF / 255f, 1f); // #D7F8FF
-    private float blendFactor = 0.75f;
-    private float alpha = 0.7f;
+    private float blendFactor = 0.50f;
+    private float alpha = 0.8f;
 
     void Awake() {
         this.player = GetComponentInParent<PlayerController>();
@@ -22,6 +22,9 @@ public class NameTag: MonoBehaviour {
     }
 
     void Update() {
-        this.textMesh.transform.LookAt(Camera.main.transform);
+        if(Camera.main) {
+            this.textMesh.transform.LookAt(Camera.main.transform);
+            this.textMesh.transform.Rotate(0, 180, 0);
+        }
     }
 }
