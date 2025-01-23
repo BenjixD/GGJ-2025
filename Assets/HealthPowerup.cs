92e874275ab94ae66eaf0266444c5a3dc3088d31
Grace Zhao <gracezhao@62-08-a2-b8-a8-58.local.tld>
2025-01-23 18:09:50 -0500

    spinning health powerup

--- Assets/HealthPowerup.cs
+++ Assets/HealthPowerup.cs
@@ -4,18 +4,34 @@ using Photon.Pun;
 public class HealthPowerup : NetworkedMonoBehaviour
 {
     public float healthAmount = 50f;
+
+    [Header("Animation")]
+    public float bobbingSpeed = 2.0f;
+    public float bobbingHeight = 0.25f;
+    public float rotationSpeed = 90.09f;
+
+    private Vector3 startPosition;
+
+    protected override void StartLocal()
+    {
+        startPosition = transform.position;
+    }
+
+    protected override void UpdateLocal()
+    {
+        float newY = startPosition.y + Mathf.Sin(Time.time * bobbingSpeed) * bobbingHeight;
+        transform.position = new Vector3(startPosition.x, newY, startPosition.z);
+
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+
+    }
+
     protected override void WriteSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-        // stream.SendNext(rb.transform.position);
     }
 
     protected override void ReadSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-        // networkedPosition = (Vector3)stream.ReceiveNext();
-
-        // Compensate for lag
-        // float lag = Mathf.Abs((float)(PhotonNetwork.Time - info.SentServerTime));
-        // networkedPosition += rb.linearVelocity * lag;
     }
 
     protected override void OnTriggerEnterLocal(Collider collision)
